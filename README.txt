a3022f99e74a68d69013175062f8e7d001004346
Lehman Black <dosnlinux@gmail.com>
2012-08-25 18:23:08 -0500

    rewrite of installer based on the svn-addon, updated to ViewVC 1.1.11
    Installer works properly. Update to ViewVC 1.1.11 required a path tweak
    in the alias files. Added BUGS.txt and a README.txt file for
    documentation.

---
+++ README.txt
@@ -0,0 +1,51 @@
+Subversion Add-on for WampServer Version 2.2a
+
+Apache version: 2.2.21
+
+About:
+ This is an addon for WampServer 2.2a that enables browsing Subversion/CVS repositories using ViewVC
+
+Where to download ViewVC:
+ Official: http://www.viewvc.org/download.html
+
+Required Addons:
+ * Python
+ * Subversion
+ * Subversion Language Bindings
+ * DiffUtils
+
+Optionally Required:
+
+Manual install instructions:
+ (assumes wamp is already installed and working)
+
+ 1. Install the required modules listed above
+ 2. download the ViewVC zip file listed above
+ 3. extract the files to a temporary directory
+ 4. inside of the folder that was extracted you should see a directory named
+    viewvc-%VERSION%
+ 5. open a command prompt window and run the installer inside of that folder
+    > python viewvc-%VERSION%\viewvc-install --prefix="c:\wamp\apps\viewvc%VERSION%" --destdir=""
+ 6. copy one of alias files provided to c:\wamp\alias and rename the file viewvc.conf. You may
+    need to change the file paths in the alias file if you are using a different version of ViewVC.
+ 7. edit the viewvc.conf file in c:\wamp\apps\viewvc%VERSION% as needed, or use the provided one.
+ 8. restart Wamp
+
+ * NOTE: The viewvc.conf provided is basically the one distributed with ViewVC with the root_parents
+  line changed to c:\svn so it will work with the Subversion add on.
+
+Using the Installer:
+ usage: installer.bat
+
+
+TODO:
+ * section on troubleshooting
+ * integration with wampserver manager menu on systray
+ * virtual hosting (viewvc.*)
+ * add abillity to run using mod_python or mod_wsgi instead of just CGI
+ * optional features like pygments and MySQL support
+
+FIXME:
+ * spelling/capitalization check on all files
+ * currently only Subversion support tested/supported
+ * alias files contain a reference to the VIEWVC_VERSION hard coded. this needs to be more generic
--- installer.bat
+++ installer.bat
@@ -1,105 +1,69 @@
 @echo OFF
 REM TODO use the call command to set some variables common to both the installer/uninstaller
-REM TODO implement download functionallity
-set VIEWVC_VERSION=1.0.12
-set APACHE_VERSION=2.2.11
-set DIFFUTILS_VERSION=2.8.7-1
+REM Requires the python, diffutils, subversion, subversion lang bindings
+set VIEWVC_VERSION=1.1.11
+set APACHE_VERSION=2.2.21
+set WAMP_VERSION=2.2a
+
+set ADDON=ViewVC
 
 set BIN=installer\bin
 set TMP=installer\temp
 
-set WAMP="c:\wamp"
-set WAMP_DIFFUTILS=%WAMP%\bin\diffutils
-
-set PYTHON="c:\python26"
-
-set DIFFUTILS_FILE=diffutils-%DIFFUTILS_VERSION%
-set DIFFUTILS_DIR=diffutils%DIFFUTILS_VERSION%
+set WAMP=c:\wamp
+set WAMP_VIEWVC=%WAMP%\apps\viewvc%VIEWVC_VERSION%
 
 set VIEWVC_FILE=viewvc-%VIEWVC_VERSION%
 set VIEWVC_DIR=viewvc%VIEWVC_VERSION%
 
-REM choose one of following apache config files for viewvc
+set VIEWVC_DOWNLOAD=http://www.viewvc.org/%VIEWVC_FILE%.zip
+
+REM choose one of following apache config files for ViewVC
 set VIEWVC_ALIAS=viewvc-cgi.conf
+REM set VIEWVC_ALIAS=viewvc-mod_wsgi.conf
 REM set VIEWVC_ALIAS=viewvc-mod_python.conf
 
 set PATH=%PATH%;%BIN%
 
-echo Welcome to the ViewVC Addon installer for WampServer 2.0i
+echo Welcome to the %ADDON% Addon installer for WampServer %WAMP_VERSION%
 
 REM set up the temp directory
 IF NOT EXIST %TMP% GOTO MKTMP
-REM FIXME: the rd command is commented out until downloading works
 echo 	Temp directory found from previous install: DELETING
-REM rd /S /Q %TMP%
+rd /S /Q %TMP%
 
 :MKTMP
 echo 	Setting up the temp directory...
 mkdir %TMP%
 
-REM download subversion archive to temp directory
-REM TODO: download not functional yet
-
-REM TODO eventually python installation will be its own addon
-echo 	Installing Python...
-REM installer\temp\python-2.6.msi
-REM installer\temp\pywin32-214.win32-py2.6.exe
-REM TODO add python to %PATH%
-
-REM TODO eventually svn python bindings installation will be its own addon
-echo 	Installing Python bindings for subversion...
-unzip.exe -q %TMP%\svn-win32-1.6.6_py26.zip -d %TMP%
-
-mkdir %TMP%\pyd
-copy %TMP%\svn-win32-1.6.6\python\libsvn\*.dll %TMP%\pyd
-for /R %%I in (%TMP%\pyd\*.dll) do ren "%%I" %%~nI.pyd
-move %TMP%\pyd\* %TMP%\svn-win32-1.6.6\python\libsvn
-
-move %TMP%\svn-win32-1.6.6\python\svn %PYTHON%\Lib
-move %TMP%\svn-win32-1.6.6\python\libsvn %PYTHON%\Lib
-
-REM unzip the downloaded source files and install them
-REM TODO download not implemented
+REM download ViewVC archive to temp directory
+echo 	Downloading %ADDON% binaries to temp directory...
+wget.exe -nd -q -P %TMP% %VIEWVC_DOWNLOAD%
 
 REM unzip the downloaded source files and install them
 echo 	Extracting the files from the downloaded archive...
 unzip.exe -q %TMP%\%VIEWVC_FILE%.zip -d %TMP%
 
-REM install the viewvc files in the WampServer install directory
-echo 	Running the ViewVC installer to put viewvc in the Apps directory...
-python %TMP%\%VIEWVC_FILE%\viewvc-install --prefix=%WAMP%\apps\%VIEWVC_DIR% --destdir=""
-
-REM install diffutils needed to view diffs in svn
-REM FIXME should this be it's own installer? Nothing else I know of uses it
-mkdir %TMP%\%DIFFUTILS_DIR%
-unzip.exe -q %TMP%\%DIFFUTILS_FILE%-dep.zip bin\* -d %TMP%\%DIFFUTILS_DIR%
-unzip.exe -q %TMP%\%DIFFUTILS_FILE%-bin.zip bin\* -d %TMP%\%DIFFUTILS_DIR%
-
-mkdir %WAMP_DIFFUTILS%
-move %TMP%\%DIFFUTILS_DIR% %WAMP_DIFFUTILS%
-
-REM FIXME add diffutils to %PATH%
+REM install the ViewVC files in the WampServer install directory
+echo 	Running the ViewVC installer to put ViewVC in the Apps directory...
+python %TMP%\%VIEWVC_FILE%\viewvc-install --prefix=%WAMP_VIEWVC% --destdir=""
 
 REM install the apache config file for ViewVC
-echo 	Installing ViewVC configuration files...
+REM FIXME: may be able to skip moving to %TMP% and just copy to %WAMP_ALIAS%
+REM FIXME: and rename during that copy command
+echo 	Installing %ADDON% configuration files...
 copy wamp\alias\%VIEWVC_ALIAS% %TMP%
 ren %TMP%\%VIEWVC_ALIAS% viewvc.conf
 move %TMP%\viewvc.conf %WAMP%\alias
 
 REM install a copy of the viewvc.conf file configured to work with WampServer
 echo 	Configuring viewvc.conf to work with WampServer...
-copy viewvc.conf %WAMP%\apps\%VIEWVC_DIR%
-
-REM add python and diffutils bin directory to the PATH so apache can find them
-echo 	Setting enviorment variables...
-REM FIXME implement
-REM licensing restrictions may prevent using xset
+copy viewvc.conf %WAMP_VIEWVC%
 
 REM clean up temp files
 echo 	Cleaning up temp files...
-REM FIXME: the rd command is commented out until downloading works
-REM rd /S /Q %TMP%
+rd /S /Q %TMP%
 
-echo ViewVC is installed successfully. Please restart WampServer.
+echo %ADDON% is installed successfully. Please restart WampServer.
 
 pause
